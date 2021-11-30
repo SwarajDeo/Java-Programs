7a5acac590d3ed03c82f2e756710f83b9c6b2ead
Swaraj Deo <84644726+SwarajDeo@users.noreply.github.com>
2021-11-30 18:05:35 +0530

    Add files via upload

---
+++ t4_literals.java
@@ -0,0 +1,18 @@
+package learnJava;
+
+public class t4_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 55;
+        char ch = 'A';
+        long ageDino = 5666666666666L;
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        String str = "Harry";
+
+        System.out.println(age);
+        System.out.println(str);
+        System.out.println(age2);
+    }
+}
